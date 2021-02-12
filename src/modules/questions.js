87920d474565a92b93994fa6dbb8c719b54c8159
Lionel Townsend <townsendlionel@gmail.com>
2021-02-11 18:31:58 -0600

    altered questions

--- src/modules/questions.js
+++ src/modules/questions.js
@@ -1,16 +1,14 @@
-let projectTitle = {title: "", body: "", required: true};
-let projectDescription = {title: "Description", body: "", required: false};
-let features = {title: "Features", body: "", required: false};
-let tableOfContents = {title: "Table of Contents", body: "", required: false};
-let installation = {title: "Installation", body: "", required: true};
-let usage = {title: "## " + "Usage", body: "", required: true};
-let license = {title: "## " + "License", body: "", required: true};
-let contributing = { title: "## " +  "Contributing", body: "", required: false};
-let credits = { title: "## " +  "Contributing", body: "", required: false};
-let tests = {title: "## Tests", body: "", required: false};
-let questions = {title: "## Questions", body: "", required: false};
-
-let badges = {}
+let projectTitle = { title: "", body: "", required: true };
+let projectDescription = { title: "Description", body: "", required: false };
+let features = { title: "Features", body: "", required: false };
+let tableOfContents = { title: "Table of Contents", body: "", required: false };
+let installation = { title: "Installation", body: "", required: true };
+let usage = { title: "## " + "Usage", body: "", required: true };
+let license = { title: "## " + "License", body: "", required: true };
+let contributing = { title: "## " + "Contributing", body: "", required: false };
+let credits = { title: "## " + "Contributing", body: "", required: false };
+let tests = { title: "## Tests", body: "", required: false };
+let questions = { title: "## Questions", body: "", required: false };
 
 let template = `
 # ${projectTitle.title}
@@ -52,77 +50,79 @@ ${license.body}
 `;
 
 
-// questions
+let questions = [
 
-let getGithubUsername = {
-    type: 'input',
-    name: 'gitHubUserName',
-    message: 'What would you like the project title to be?' 
-}
+    {
+        type: 'input',
+        name: 'gitHubUserName',
+        message: 'What would you like the project title to be?'
+    },
 
-let getProjectTitle = {
-    type: 'input',
-    name: 'projectTitle',
-    message: 'What would you like the project title to be?' 
-}
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What would you like the project title to be?'
+    },
 
-let getProjectDescription = {
-    type: 'editor',
-    name: 'projectDescription',
-    message: 'How would you describe ypur project?' 
-}
+    {
+        type: 'editor',
+        name: 'projectDescription',
+        message: 'How would you describe ypur project?'
+    },
 
-let getFeatures = {
-    type: 'editor',
-    name: 'features',
-    message: 'What features does your project have?' 
-}
+    {
+        type: 'editor',
+        name: 'features',
+        message: 'What features does your project have?'
+    },
 
-let getTableOfContents = {
-    type: 'input',
-    name: 'tableOfContents',
-    message: 'Do you want the readme to have a table of contents?' 
-}
+    {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'Do you want the readme to have a table of contents?'
+    },
 
-let getInstallationInstructions = {
-    type: 'editor',
-    name: 'installationInstructions',
-    message: 'What are your installation instructions' 
-}
+    {
+        type: 'editor',
+        name: 'installationInstructions',
+        message: 'What are your installation instructions'
+    },
 
-let getUsage = {
-    type: 'editor',
-    name: 'usageInstructions',
-    message: 'What are your useage instructions' 
-}
+    {
+        type: 'editor',
+        name: 'usageInstructions',
+        message: 'What are your useage instructions'
+    },
 
-let getLicense = {
-    type: 'choice',
-    name: 'licenseType',
-    message: 'What type of license will this project use',
-    choices: []
-}
+    {
+        type: 'choice',
+        name: 'licenseType',
+        message: 'What type of license will this project use',
+        choices: []
+    },
 
-let getContributing = {
-    type: 'input',
-    name: 'contributions',
-    message: 'What are your contribution guidelines?' 
-}
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'What are your contribution guidelines?'
+    },
 
-let getCredits = {
-    type: 'editor',
-    name: 'credits',
-    message: 'Who would you like to give credit to?' 
-}
+    {
+        type: 'editor',
+        name: 'credits',
+        message: 'Who would you like to give credit to?'
+    },
+    {
+        type: 'editor',
+        name: 'testInstructions',
+        message: 'What are your test instructions?'
+    },
 
-let getTests = {
-    type: 'editor',
-    name: 'testInstructions',
-    message: 'What are your test instructions?'
-}
-
-let getQuestions = {
-    type: 'input',
-    name: 'questions',
-    message: 'What email should questions be sent to?' 
-}
+    {
+        type: 'input',
+        name: 'questionsForYou',
+        message: 'What email should questions be sent to?'
+    }
+]
+console.log(questions);
+module.exports = questions;
