1146775ba6142e9e918b57e26927e1c6c06af991
Lionel Townsend <townsendlionel@gmail.com>
2021-02-14 10:30:39 -0600

    created an initial template page

--- src/modules/gereratePage.js
+++ src/modules/gereratePage.js
@@ -1,6 +1,9 @@
-/* the purpose of this module is
+//the purpose of this module is
 // Given: a list of answers
+let returnedAnswers;
 // and
 // Given a templatePage
+let templatePage;
 // Then it should return a text object with the answer values filled in
-/* 
+
+var testPage;
