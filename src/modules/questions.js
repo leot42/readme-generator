d91735b131651ccffabc07a2bdba12b71b4e2105
Lionel Townsend <townsendlionel@gmail.com>
2021-02-14 12:16:57 -0600

    questions.js - added some validation

--- src/modules/questions.js
+++ src/modules/questions.js
@@ -1,21 +1,30 @@
+// validate that user input is not an empty answer or just spaces
+function IsAnswerGivenNotEmpty (userInput) {
+    if (userInput == "true") return true;
+    else return "You must give an answer.";
+}
+
+// the set of questions to ask
 let questionSet = [
 
     {
         type: 'input',
         name: 'gitHubUserName',
-        message: 'What is the GitHub user name to attach to the project?'
+        message: 'What is the GitHub user name to attach to the project? (Required)'
     },
 
     {
         type: 'input',
         name: 'projectTitle',
-        message: 'What would you like the project title to be?'
+        message: 'What would you like the project title to be? (Required)',
+        validate: (answer) => IsAnswerGivenNotEmpty(answer)
     },
 
     {
         type: 'editor',
         name: 'projectDescription',
-        message: 'How would you describe your project?'
+        message: 'How would you describe your project? (Required)',
+        validate: (answer) => IsAnswerGivenNotEmpty(answer)
     },
 
     {
@@ -27,32 +36,37 @@ let questionSet = [
     {
         type: 'confirm',
         name: 'tableOfContents',
-        message: 'Do you want the readme to have a table of contents?'
+        message: 'Do you want the readme to have a table of contents? (Required)',
+        validate: (answer) => IsAnswerGivenNotEmpty(answer)
     },
 
     {
         type: 'editor',
         name: 'installationInstructions',
-        message: 'What are your installation instructions'
+        message: 'What are your installation instructions? (Required)',
+        validate: (answer) => IsAnswerGivenNotEmpty(answer)
     },
 
     {
         type: 'editor',
         name: 'usageInstructions',
-        message: 'What are your useage instructions'
+        message: 'What are your useage instructions? (Required)',
+        validate: (answer) => IsAnswerGivenNotEmpty(answer)
     },
 
     {
         type: 'choice',
         name: 'licenseType',
-        message: 'What type of license will this project use',
-        choices: []
+        message: 'What type of license will this project use? (Required)',
+        choices: [],
+        validate: (answer) => IsAnswerGivenNotEmpty(answer)
     },
 
     {
         type: 'editor',
         name: 'contributions',
-        message: 'What are your contribution guidelines?'
+        message: 'What are your contribution guidelines? (Required)',
+        validate: (answer) => IsAnswerGivenNotEmpty(answer)
     },
 
     {
@@ -63,13 +77,15 @@ let questionSet = [
     {
         type: 'editor',
         name: 'testInstructions',
-        message: 'What are your test instructions?'
+        message: 'What are your test instructions? (Required)',
+        validate: (answer) => IsAnswerGivenNotEmpty(answer)
     },
 
     {
         type: 'input',
         name: 'questionsForYou',
-        message: 'What email should questions be sent to?'
+        message: 'What email should questions be sent to? (Required)',
+        validate: (answer) => IsAnswerGivenNotEmpty(answer)
     }
 ]
 // console.log(questionSet);
