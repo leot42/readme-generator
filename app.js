5190532d7f47e726359e49b6b8a0da72aae13d77
Lionel Townsend <townsendlionel@gmail.com>
2021-02-16 13:46:10 -0600

    found and used suggested assets. replaced generatePage.js file

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 let badgeData = require('./src/modules/badges.js');
 let inquirer = require('inquirer');
-let questionSet = require('./src/modules/questions')
+const questionSet = require('./src/modules/questions')
 let userInterface = require('./src/modules/userInterface')
 
 // async cliPrompt (questionSet) returns 
 cliPrompt = inquirer.prompt(questionSet).then((answers) => {
     console.log(JSON.stringify(answers, null, '  '));
   });
-  
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
--- src/modules/gereratePage.js
+++
@@ -1,9 +0,0 @@
-    // Given: a list of answers
-    // and
-    // Given a templatePage
-let GeneratePage = (returnedAnswers, pageTemplate) => {
-    
-    // pageTemplate.m
-    // Then it should return a text object with the answer values filled in
-    var testPage;
-}
